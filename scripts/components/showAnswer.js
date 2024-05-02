3cfb38476bd94d3fade5b5ab108d8bed7f4cc223
Paul Thomas <traitorpt@gmail.com>
2024-05-01 22:52:18 -0700

    added lock screen after answer, darkened screen on wrong answer, make correct answer white

--- scripts/components/showAnswer.js
+++ scripts/components/showAnswer.js
@@ -1,32 +1,59 @@
+function lockScreen() {
+    const overlay = document.createElement('div');
+    overlay.className = 'overlay';
+    document.body.appendChild(overlay);
+}
+
+function unlockScreen() {
+    const overlay = document.querySelector('.overlay');
+    if (overlay) {
+        overlay.parentNode.removeChild(overlay);
+    }
+}
+
 function answerIsCorrect(correctAnswerIndex, nextQuestionCallback) {
+    lockScreen(); // Lock the screen
+    const questionCard = document.querySelector('.question-card');
     const correctOption = document.querySelectorAll('.answer-option')[correctAnswerIndex];
     if (correctOption) {
         correctOption.style.backgroundColor = 'green';
-        setTimeout(() => {
+        correctOption.style.color = '#ffffff';
+        // questionCard.style.backgroundColor = 'grey';
+        setTimeout(() => {        
+            questionCard.style.backgroundColor = '';
             correctOption.style.backgroundColor = ''; // Reset to original background color
+            unlockScreen(); // Unlock the screen
             nextQuestionCallback();
         }, 3000);
     } else {
         console.error('Correct option not found in the DOM');
+        unlockScreen(); // Unlock the screen in case of error
         nextQuestionCallback();
     }
 }
 
 function answerIsWrong(selectedAnswerIndex, correctAnswerIndex, nextQuestionCallback) {
+    lockScreen(); // Lock the screen
+    const questionCard = document.querySelector('.question-card');
     const correctOption = document.querySelectorAll('.answer-option')[correctAnswerIndex];
     const selectedOption = document.querySelectorAll('.answer-option')[selectedAnswerIndex];
     if (correctOption && selectedOption) {
+        questionCard.style.backgroundColor = 'grey';
         correctOption.style.backgroundColor = 'green';
+        correctOption.style.color = '#ffffff';
         selectedOption.style.backgroundColor = 'red';
         setTimeout(() => {
+            questionCard.style.backgroundColor = '';
             correctOption.style.backgroundColor = ''; // Reset to original background color
             selectedOption.style.backgroundColor = ''; // Reset to original background color
+            unlockScreen(); // Unlock the screen
             nextQuestionCallback();
         }, 3000);
     } else {
         console.error('Correct or selected option not found in the DOM');
+        unlockScreen(); // Unlock the screen in case of error
         nextQuestionCallback();
     }
 }
 
-export { answerIsCorrect, answerIsWrong}
+export { answerIsCorrect, answerIsWrong };
